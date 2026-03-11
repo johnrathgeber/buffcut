a15ed99236c64175e5e76169e1a6c9d61bc8c4c1
John Rathgeber <john_rathgeber@brown.edu>
2026-03-10 21:16:19 -0400

    protein shake calories adjusted

--- lib/types.ts
+++ lib/types.ts
@@ -29,7 +29,7 @@ export const PRESET_FOODS = [
   { name: 'Chicken Breast', protein: 22, calories: 134 },
   { name: 'Turkey Patty', protein: 20, calories: 158 },
   { name: 'Protein Bar', protein: 28, calories: 150 },
-  { name: 'Protein Shake', protein: 24, calories: 130 },
+  { name: 'Protein Shake', protein: 24, calories: 120 },
   { name: 'Plate of Veggies', protein: 2, calories: 50 },
   { name: 'Hard Boiled Egg', protein: 6, calories: 78 },
 ] as const;
